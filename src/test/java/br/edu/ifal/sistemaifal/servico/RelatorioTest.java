0bbeda638a9f1362a6be185425edf49c1504dfbd
Priscylla Silva <mss.priscylla@gmail.com>
2018-03-06 22:55:15 -0300

    Aula de Testes 1.8.2

--- src/test/java/br/edu/ifal/sistemaifal/servico/RelatorioTest.java
+++ src/test/java/br/edu/ifal/sistemaifal/servico/RelatorioTest.java
@@ -69,5 +69,63 @@ public class RelatorioTest {
 		Assert.assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
 		
 	}
+	
+	@Test
+	public void deveEntenderAsNotasEmOrdemDecrescente() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(1, "2018A1", "Zé");
+		Aluno ana = new Aluno(2, "2018A2", "Ana");
+		Aluno joao = new Aluno(3, "2018A3", "João");
+		alunos.add(ze);
+		alunos.add(ana);
+		alunos.add(joao);
+		
+		Professor maria = new Professor(1, "Maria", "informática");
+		
+		Disciplina poo = new Disciplina(1, "POO", alunos, maria);
+		
+		List<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1, ze, poo, 7.0));
+		notas.add(new Nota(1, ana, poo, 4.5));
+		notas.add(new Nota(1, joao, poo, 2.0));
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+		
+		double maiorNotaEsperada = 7.0;
+		double menorNotaEsperada = 2.0;
+		
+		Assert.assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		Assert.assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+	}
+	
+	@Test
+	public void deveEntenderAsNotasEmOrdemAleatoria() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(1, "2018A1", "Zé");
+		Aluno ana = new Aluno(2, "2018A2", "Ana");
+		Aluno joao = new Aluno(3, "2018A3", "João");
+		alunos.add(ze);
+		alunos.add(ana);
+		alunos.add(joao);
+		
+		Professor maria = new Professor(1, "Maria", "informática");
+		
+		Disciplina poo = new Disciplina(1, "POO", alunos, maria);
+		
+		List<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1, ze, poo, 4.5));
+		notas.add(new Nota(1, ana, poo, 2.0));
+		notas.add(new Nota(1, joao, poo, 7.0));
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+		
+		double maiorNotaEsperada = 7.0;
+		double menorNotaEsperada = 2.0;
+		
+		Assert.assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		Assert.assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+	}
 
 }
