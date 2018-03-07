649d5482517bb628476920096c18e4bfc75463e4
Priscylla Silva <mss.priscylla@gmail.com>
2018-03-06 22:52:48 -0300

    Aula de Testes 1.8.1

--- src/test/java/br/edu/ifal/sistemaifal/servico/RelatorioTest.java
+++ src/test/java/br/edu/ifal/sistemaifal/servico/RelatorioTest.java
@@ -43,5 +43,31 @@ public class RelatorioTest {
 		Assert.assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
 		Assert.assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
 	}
+	
+	@Test
+	public void deveEntenderUmUnicaNota(){
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(1, "2018A1", "Zé");
+		alunos.add(ze);
+		
+		Professor maria = new Professor(1, "Maria", "informática");
+		
+		Disciplina poo = new Disciplina(1, "POO", alunos, maria);
+		
+		List<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1, ze, poo, 2.0));
+
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+		
+		double maiorNotaEsperada = 2.0;
+		double menorNotaEsperada = 2.0;
+		
+		Assert.assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		Assert.assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+		
+	}
 
 }
